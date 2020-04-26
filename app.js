34067758edbb58fe6a7dc7cf15ec294285987b2a
javaangular1 <javaangular1@gmail.com>
2020-04-26 11:23:39 +0200

    Added login feature

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 // Proje1 again - conflict resolved
+
 let commands = ["config", "init", "add", "commit", "pull", "push", "reset", "rm"];
 let branchCommands = ["branch", "checkout"]
+
+// TODO login commands will be here
